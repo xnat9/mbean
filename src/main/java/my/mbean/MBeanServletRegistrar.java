5e1958c32257578b85fa04903c7bb7747287a850
xnatural@msn.cn <xnat9,./>
2017-05-24 18:49:22 +0800

    tmp...

--- src/main/java/my/mbean/MBeanServletRegistrar.java
+++ src/main/java/my/mbean/MBeanServletRegistrar.java
@@ -1,11 +1,16 @@
 package my.mbean;
 
+import my.mbean.util.Log;
 import my.mbean.web.MBeanServlet;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.EnumSet;
 
 import static my.mbean.AutoConfiguration.MBEAN_SERVLET_NAME;
 
@@ -13,6 +18,10 @@ import static my.mbean.AutoConfiguration.MBEAN_SERVLET_NAME;
  * Created by Administrator on 2017/5/18.
  */
 public class MBeanServletRegistrar implements WebApplicationInitializer {
+
+    private static final EnumSet<DispatcherType> ASYNC_DISPATCHER_TYPES = EnumSet.of(
+            DispatcherType.FORWARD, DispatcherType.INCLUDE, DispatcherType.REQUEST,
+            DispatcherType.ASYNC);
     @Override
     public void onStartup(ServletContext pServletContext) throws ServletException {
         MBeanServlet mbeanServlet = new MBeanServlet();
@@ -26,5 +35,16 @@ public class MBeanServletRegistrar implements WebApplicationInitializer {
 //
 //        }
         registration.addMapping(MBeanConfiguration.MBEAN_URL_PREFIX + "/*");
+
+//         add filter.
+        FilterRegistration.Dynamic dynamic = pServletContext.addFilter("webRequestLoggingFilter", new OncePerRequestFilter() {
+            Log log = Log.of("webRequestLoggingFilter");
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                log.info("trace request: uri: {0.requestURI}, method: {0.method}, remoteAddr: {0.remoteAddr}", request);
+                filterChain.doFilter(request, response);
+            }
+        });
+        dynamic.addMappingForUrlPatterns(ASYNC_DISPATCHER_TYPES, false, "/*");
     }
 }
--- src/main/java/my/mbean/spring/autoconfigure/CommonSpringBeanConfiguration.java
+++ src/main/java/my/mbean/spring/autoconfigure/CommonSpringBeanConfiguration.java
@@ -1,12 +1,15 @@
 package my.mbean.spring.autoconfigure;
 
 import my.mbean.spring.CustomBeanNameGenerator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
 
 /**
  * for common bean define.
@@ -21,6 +24,18 @@ public class CommonSpringBeanConfiguration {
     @EventListener(ContextRefreshedEvent.class)
     void postApplicationReady(ContextRefreshedEvent pRefreshedEvent) {
         registerSpringCommonBean(pRefreshedEvent);
+    }
+
+    @EventListener(ServletRequestHandledEvent.class)
+    void postProcessRequest(ServletRequestHandledEvent event) {
+        if (environment.getProperty("spring.request.monitor", boolean.class, true)) {
+            Log logger = LogFactory.getLog(event.getSource().getClass());
+            if (event.getProcessingTimeMillis() > 1500) {
+                logger.warn("long request: " + event.toString());
+            } else {
+                logger.info(event.toString());
+            }
+        }
     }
 
 
